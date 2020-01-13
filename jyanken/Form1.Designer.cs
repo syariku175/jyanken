07d3143cca47c30ae028dab42eed7ec7c9a243ae
syariku175 <syariku@hotmail.com>
2020-01-13 23:22:34 +0900

    プロジェクト ファイルを追加します。

---
+++ jyanken/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace jyanken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ラベルをクリック";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(243, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 228);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "グー";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "チョキ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(508, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "パー";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ jyanken/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jyanken
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string comphand = this.Getcomphand();
+
+            this.Hantei("グー", comphand);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string comphand = this.Getcomphand();
+
+            this.Hantei("チョキ", comphand);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string comphand = this.Getcomphand();
+
+            this.Hantei("パー", comphand);
+
+        }
+
+        private string Getcomphand()
+        {
+            Random rand = new Random();
+
+            int result = rand.Next(3);
+
+            string resultstring = "";
+
+            switch (result)
+            {
+                case 0:
+
+                    label1.Text = "コンピューターはグー";
+
+                    pictureBox1.ImageLocation = "image/g.png";
+
+                    resultstring = "グー";
+
+                    break;
+
+                case 1:
+
+                    label1.Text = "コンピューターはチョキ";
+
+                    pictureBox1.ImageLocation = "image/c.png";
+
+                    resultstring = "チョキ";
+
+
+                    break;
+
+                case 2:
+
+                    label1.Text = "コンピューターはパー";
+
+                    pictureBox1.ImageLocation = "image/p.png";
+
+                    resultstring = "パー";
+
+                    break;
+
+            }
+
+            return resultstring;
+
+        }
+
+        private void Hantei(string myhand, string comphand)
+        {
+
+            if (myhand == comphand)
+            {
+                label1.Text += " / 引き分けです";
+
+            }
+
+            else if(myhand == "グー" && comphand == "チョキ" ||
+                    myhand == "チョキ" && comphand == "パー" ||
+                    myhand == "パー" && comphand == "グー"
+                )
+            {
+                label1.Text += "/ 勝ちました";
+            }
+
+            else
+            {
+                label1.Text += " / 負けました";
+            }
+        }
+
+    }
+}
